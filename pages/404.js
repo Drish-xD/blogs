da9d2f553ae22fee07797a4fbf9e0e012136578e
Drish-xD <drish.xd@gmail.com>
2022-02-25 17:18:16 +0530

    added custom 404

---
+++ pages/404.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Custom404() {
+  return (
+    <div className="container mx-auto mb-8 px-10`">
+      <Head>
+        <title>Page Not Found</title>
+        <meta name="description" content="Error 404!!! Page Not Found" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="container mx-auto flex justify-center items-center flex-col">
+        <Image src="/Images/404.png" alt="404" width="600" height="600" loading="lazy" />
+      </div>
+    </div>
+  );
+}
