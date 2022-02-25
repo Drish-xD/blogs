466b085b1405ad4d7be3f84f79d79ffa0cfb8e1f
Drish-xD <drish.xd@gmail.com>
2022-02-25 22:45:07 +0530

    added author

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,7 +1,24 @@
+import Image from "next/image";
 import React from "react";
 
 const Author = ({ author }) => {
-  return <div>{author.name}</div>;
+  return (
+    <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
+      <div className="absolute left-0 right-0 -top-14">
+        <Image
+          src={author.photo.url}
+          alt={author.name}
+          height={100}
+          width={100}
+          objectFit="cover"
+          loading="lazy"
+          className="rounded-full"
+        />
+      </div>
+      <h3 className="text-white my-4 text-xl font-bold">{author.name}</h3>
+      <p className="text-lg text-gray-100">{author.bio}</p>
+    </div>
+  );
 };
 
 export default Author;
